4db739f3cc4f80c1e1f6b4063ed72a794771d904
Dima Zeland <onenewinvest@gmail.com>
2018-12-22 00:01:50 +0200

    Add files via upload

---
+++ 2018.12.15 Overloading operations/Exercise 2/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include"Tv.h"
+
+int main() {
+	Tv Sony;
+	Remote Tele;
+
+	Sony.Power();
+	Sony.incVolume();
+	Sony.nextChannel();
+	Tele.goToChannel(Sony, 48);
+	Tele.prevChannel(Sony);
+	Tele.incVolume(Sony);
+	Tele.power(Sony);
+
+	system("pause");
+
+	return 0;
+}
